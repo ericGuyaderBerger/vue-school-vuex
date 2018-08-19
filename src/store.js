bcfd26759d1fb15e2952f6eeb980807c11e847d3
Eric Guyader <ericguyader@yahoo.fr>
2018-08-19 19:27:38 +0200

    Initializes VueX store

--- src/store.js
+++ src/store.js
@@ -5,15 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: { // = data
-
+    products: []
   },
   getters: { // = computed
-
+    productsCount () {
+      return this.store.products.length
+    }
   },
   actions: { // = methods
+    fetchProducts () {
 
+    }
   },
   mutations: { // = nothing ;-) !
+    setProducts () {
 
+    }
   }
 })
