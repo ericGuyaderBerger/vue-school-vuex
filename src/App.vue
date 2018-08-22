71448f13ee0fcde0587009327851f8a7d5fade2b
Eric Guyader <ericguyader@yahoo.fr>
2018-08-22 10:00:55 +0200

    Adds shopping cart component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <product-list />
+    <hr>
+    <shopping-cart />
   </div>
 </template>
 
 <script>
 import ProductList from './components/ProductList'
+import ShoppingCart from './components/ShoppingCart'
 
 export default {
   name: 'app',
   components: {
-    ProductList
+    ProductList,
+    ShoppingCart
   }
 }
 </script>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <ul>
+      <li v-for="product in products"
+        :key="product.id">
+        {{ product.title }} - {{ product.price }} - {{ product.quantity }}
+      </li>
+    </ul>
+    <p>Total: {{ total }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ShoppingCart',
+  computed: {
+    total () { return this.$store.getters.cartTotal },
+    products () { return this.$store.getters.cartProducts }
+  }
+}
+</script>
